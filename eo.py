9940e458565b1583c685d81bf18f91aa10bdfcdb
pradyumna17 <pradyumnapardeshi17@gmail.com>
2026-03-23 22:34:29 +0530

    Pradyumna 2

---
+++ eo.py
@@ -0,0 +1,8 @@
+# Taking user input
+num = int(input("Enter a number: "))
+
+# Checking if the number is even or odd
+if num % 2 == 0:
+    print(f"{num} is an Even number.")
+else:
+    print(f"{num} is an Odd number.")
